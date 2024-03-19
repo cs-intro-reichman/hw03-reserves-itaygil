11971a4753a082dd18d55952d8f7098a6445794d
itay.giladigit config --global user.email itay.giladi@post.runi.ac.ilgit config --global user.name Your <you@example.com>
2024-03-19 11:03:40 +0200

    v2

--- Calendar0.java
+++ Calendar0.java
@@ -9,6 +9,7 @@ public class Calendar0 {
 		int year = Integer.parseInt(args[0]);
 		isLeapYearTest(year);
 		nDaysInMonthTest(year);
+		
 	}
 		 
 	// Tests the isLeapYear function.
@@ -22,13 +23,31 @@ public class Calendar0 {
 
 	// Tests the nDaysInMonth function.
 	private static void nDaysInMonthTest(int year) {
-		// Replace this comment with your code
+		String feb = "Month 2 has 28 days";
+		//check if the year is a leap year if it is assign feb t0 29
+		if (isLeapYear(year)){
+			feb = "Month 2 has 29 days";	
+		}
+			System.out.println("Month 1 has 31 days");
+			System.out.println(feb);
+			System.out.println("Month 3 has 31 days");
+			System.out.println("Month 4 has 30 days");
+			System.out.println("Month 5 has 31 days");
+			System.out.println("Month 6 has 30 days");
+			System.out.println("Month 7 has 31 days");
+			System.out.println("Month 8 has 31 days");
+			System.out.println("Month 9 has 30 days");
+			System.out.println("Month 10 has 31 days");
+			System.out.println("Month 11 has 30 days");
+			System.out.println("Month 12 has 31 days");
 	}
 
 	// Returns true if the given year is a leap year, false otherwise.
 	public static boolean isLeapYear(int year) {
-	    // Replace the following statement with your code
-		return false;
+	    boolean isLeapyear;
+		isLeapyear = ((year % 400) == 0);
+		isLeapyear = isLeapyear || (((year%4)==0)&&(year%100)!=0);
+		return isLeapyear;
 	}
 	 
 	// Returns the number of days in the given month and year.
@@ -36,7 +55,8 @@ public class Calendar0 {
 	// February has 28 days in a common year, and 29 days in a leap year.
 	// All the other months have 31 days.
 	public static int nDaysInMonth(int month, int year) {
-		// Replace the following statement with your code
+		
+
 		return 0;
 	}
 }
